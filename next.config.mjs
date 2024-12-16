07ee4203a9043c4c14dbb383b022d9126ef3bd0f
Hugo da Silva Bertoncelo <hugobertoncelo@gmail.com>
2024-12-16 16:38:44 -0300

    adjusts image

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    images: {
-      allowfutureImage: true,
-    }
-  }
-}
+};
 
 export default nextConfig;
